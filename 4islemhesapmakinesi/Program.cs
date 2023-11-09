a685e4cc5ed5f50a0a81025368a156d6094a29e0
Medeni Aba <medeniabax@gmail.com>
2023-11-09 14:37:04 +0300

    Proje dosyası ekle.

---
+++ 4islemhesapmakinesi/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine("Birinci Sayıyı Giriniz ");
+int sayi1 = Int32.Parse(Console.ReadLine());
+Console.WriteLine("İkinici Sayıyı Giriniz");
+int sayi2 = Int32.Parse(Console.ReadLine());
+double sonuc;
+String a;
+Console.WriteLine("Bir operatör Seçiniz : + , - , * , / ");
+a = Console.ReadLine();
+if (a == "+")
+{
+    Console.WriteLine(sonuc = sayi1 + sayi2);
+}
+else if (a == "-")
+{
+    Console.WriteLine(sonuc = sayi1 - sayi2);
+}
+else if (a == "*")
+{
+    Console.WriteLine(sonuc = sayi1 * sayi2);
+}
+else if (a == "/")
+{
+    Console.WriteLine(sonuc = (sayi1 / sayi2));
+}
